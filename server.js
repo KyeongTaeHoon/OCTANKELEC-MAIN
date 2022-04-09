99ea4e4ee5dd748dd8958231495551fd377bffe8
KyeongTaeHoon <taehooon@3c0630355cc2.ant.amazon.com>
2022-04-09 18:14:43 +0900

    latest

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const db_config = require(__dirname + '/database.js');
 const express = require('express');
 const app = express();
 var AWS = require ('aws-sdk');
-
-AWS.config = new AWS.Config();
 AWS.config.update({region:'ap-northeast-2'});
-
 const bodyParser = require('body-parser');
 const personalizeruntime = new AWS.PersonalizeRuntime();
 const personalizeevents = new AWS.PersonalizeEvents();
